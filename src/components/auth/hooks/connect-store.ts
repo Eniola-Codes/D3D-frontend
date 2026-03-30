cedfdf35e9754e3039735ca205000783b1d0b7f0
Odunmbaku Eniola <eniolacodes1@gmail.com>
2026-03-30 16:32:03 +0100

    shopify implementation changes

--- src/components/auth/hooks/connect-store.ts
+++ src/components/auth/hooks/connect-store.ts
@@ -1,8 +1,8 @@
-import { endpoints } from "@/lib/constants/endpoints";
+import { endpoints } from '@/lib/constants/endpoints';
 
 export function useConnectStore() {
-  const handleConnect = async (shop: string) => { 
-    const url = `${process.env.NEXT_PUBLIC_API_URL}${endpoints.shopify.base}${endpoints.shopify.init}?shop=${shop}`
+  const handleConnect = async (shop: string) => {
+    const url = `${process.env.NEXT_PUBLIC_API_URL}${endpoints.shopify.base}${endpoints.shopify.init}?shop=${shop}`;
     window.location.href = url;
   };
 
