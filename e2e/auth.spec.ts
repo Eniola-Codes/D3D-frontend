a1dfc970c8a9441391bc33a61b97e25920accf3b
Odunmbaku Eniola <eniolacodes1@gmail.com>
2026-07-07 01:21:46 +0100

    Auth e2e tests

--- e2e/auth.spec.ts
+++ e2e/auth.spec.ts
@@ -516,42 +516,6 @@ test('should reject invalid reset password credentials client-side', async ({
   await expect(page.locator('body')).not.toContainText(WELCOME_BACK);
 });
 
-// test('should logout successfully', async ({ page }: { page: Page }) => {
-//   const password = '12345678';
-
-//   await page.goto(process.env.BASE_URL as string);
-//   await page.getByRole('button', { name: 'Get Started' }).click();
-//   await page.getByRole('button', { name: 'login' }).click();
-//   await page.getByRole('textbox', { name: 'Email' }).click();
-//   await page.getByRole('textbox', { name: 'Email' }).fill(email);
-//   await page.getByRole('textbox', { name: 'Password' }).click();
-//   await page.getByRole('textbox', { name: 'Password' }).fill(password);
-//   await page.getByRole('button', { name: 'login' }).click();
-
-//   await page.locator('span[data-slot="breadcrumb-page"]').waitFor();
-//   await page.getByTestId('dropdown-menu-trigger').waitFor();
-//   await page.getByTestId('dropdown-menu-trigger').click();
-
-//   await page.getByText('Log out', { exact: true }).waitFor();
-//   await page.getByText('Log out', { exact: true }).click();
-
-// console.log('URL:', page.url());
-// console.log(await page.locator('body').innerText());
-// await page.screenshot({ path: 'test-results/debug-after-logout.png', fullPage: true });
-
-//   await expect(
-//     page.locator('div[data-slot="dropdown-menu-label"] div div span').first()
-//   ).toHaveText(name);
-//   await expect(page.locator('div[data-slot="dropdown-menu-label"] div div span').last()).toHaveText(
-//     email
-//   );
-//   // await page.getByText(WELCOME_BACK).waitFor();
-//   // await expect(page.locator('body')).toContainText(WELCOME_BACK);
-//   // await page.goto(`${process.env.BASE_URL}${routes.dashboard.path.base}`);
-//   // await page.getByText(WELCOME_BACK).waitFor();
-//   // await expect(page.locator('body')).toContainText(WELCOME_BACK);
-// });
-
 test('should redirect on protected routes', async ({ page }: { page: Page }) => {
   const password = '12345678';
 
@@ -569,22 +533,13 @@ test('should redirect on protected routes', async ({ page }: { page: Page }) => 
 
   await page.locator('span[data-slot="breadcrumb-page"]').waitFor();
 
+  await page.goto(process.env.BASE_URL + routes.account.path.base + '?auth=' + routes.account.query.login);
+
   console.log('URL:', page.url());
   console.log(await page.locator('body').innerText());
   await page.screenshot({ path: 'test-results/debug-after-logout.png', fullPage: true });
 
-  await page.goto(process.env.BASE_URL + routes.account.path.base + '?auth=' + routes.account.query.login);
-
   await page.locator('span[data-slot="breadcrumb-page"]').waitFor();
-
-  await expect(page.getByTestId('dropdown-menu-trigger')).toBeVisible();
   await expect(page.locator('span[data-slot="breadcrumb-page"]')).toHaveText('Dashboard');
   await expect(page.getByRole('link', { name: 'd3d Enterprise' })).toBeVisible();
-  await page.getByTestId('dropdown-menu-trigger').click();
-  await expect(
-    page.locator('div[data-slot="dropdown-menu-label"] div div span').first()
-  ).toHaveText(name)
-  await expect(page.locator('div[data-slot="dropdown-menu-label"] div div span').last()).toHaveText(
-    email
-  );
 });
